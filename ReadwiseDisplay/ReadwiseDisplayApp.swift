d945581cf30dacbf9d4203f97e95d1e85f50b540
Sean McMains <sean@mcmains.net>
2025-05-21 20:05:22 -0500

    Initial Commit

---
+++ ReadwiseDisplay/ReadwiseDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadwiseDisplayApp.swift
+//  ReadwiseDisplay
+//
+//  Created by Sean Mc Mains on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ReadwiseDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReadwiseDisplayTests/ReadwiseDisplayTests.swift
@@ -0,0 +1,16 @@
+//
+//  ReadwiseDisplayTests.swift
+//  ReadwiseDisplayTests
+//
+//  Created by Sean Mc Mains on 5/21/25.
+//
+
+import Testing
+
+struct ReadwiseDisplayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
